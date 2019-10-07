1fe91572208e84326d1cbb9f318731f0becb3606
NKwatra <kwatranishkarsh@gmail.com>
2019-10-07 22:17:57 +0530

    Store user's number in local storage and update UI

--- Public/messages.js
+++ Public/messages.js
@@ -1,8 +1,14 @@
 $(function(){
-    var socket = io.connect("https://simple-chat12.herokuapp.com");
+    // var socket = io.connect("https://simple-chat12.herokuapp.com");
+    $("#chatroom").scrollTop($("#chatroom")[0].scrollHeight);    
+    if($("#chat-header").text() == "")
+        $("#chat-header").text(localStorage.getItem("phone"));
+    var socket = io.connect("localhost:3000");
     $("#sendMessageButton").click(function(){
         var Message = $("#message").val();
-        $("#chatroom").append(`<div class="m-3 text-right"><span class="px-4 py-2 bg-executive rounded">${Message}</span></div>`);
+        $("#message").val("");
+        $("#chatroom").append(`<div class="m-3"><span class="p-2 bg-executive rounded">${Message}</span></div>`);
+        $("#chatroom").scrollTop($("#chatroom")[0].scrollHeight);
         socket.emit('new_message', {message: Message});
     })
     
@@ -13,11 +19,14 @@ $(function(){
         }
         if(data.from != null)
         {
-            $("#chat-header").text(data.from.replace(/[^0-9+]/gi,""));
+            var phoneNo = data.from.replace(/[^0-9+]/gi,"");
+            $("#chat-header").text(phoneNo);
+            localStorage.setItem("phone", phoneNo);
         }
         $("#chat-header").addClass("collapsed");
         $("#chatwindow").addClass("show");
         $("#chatroom").append(`<div class="m-3"><span class="bg-user px-4 py-2 rounded">${data.message}</span></div>`);
+        $("#chatroom").scrollTop($("#chatroom")[0].scrollHeight);
     });
 
     if($("#chatroom").children().length == 0)
